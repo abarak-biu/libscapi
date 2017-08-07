e617c9f749d8440e29ed10ecdfad957b30e10628
liorbiu <liork.cryptobiu@gmail.com>
2017-08-07 12:50:01 +0300

    add relic folder

---
+++ lib/EMP/relic/bench/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /Users/center/relic/bench
+# Build directory: /Users/center/relic/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(bench_err "/Users/center/relic/bin/bench_err")
+add_test(bench_bn "/Users/center/relic/bin/bench_bn")
+add_test(bench_dv "/Users/center/relic/bin/bench_dv")
+add_test(bench_fp "/Users/center/relic/bin/bench_fp")
+add_test(bench_fpx "/Users/center/relic/bin/bench_fpx")
+add_test(bench_fb "/Users/center/relic/bin/bench_fb")
+add_test(bench_fbx "/Users/center/relic/bin/bench_fbx")
+add_test(bench_ep "/Users/center/relic/bin/bench_ep")
+add_test(bench_epx "/Users/center/relic/bin/bench_epx")
+add_test(bench_eb "/Users/center/relic/bin/bench_eb")
+add_test(bench_ed "/Users/center/relic/bin/bench_ed")
+add_test(bench_ec "/Users/center/relic/bin/bench_ec")
+add_test(bench_pp "/Users/center/relic/bin/bench_pp")
+add_test(bench_pc "/Users/center/relic/bin/bench_pc")
+add_test(bench_cp "/Users/center/relic/bin/bench_cp")
+add_test(bench_rand "/Users/center/relic/bin/bench_rand")
---
+++ lib/EMP/relic/test/CTestTestfile.cmake
@@ -0,0 +1,24 @@
+# CMake generated Testfile for 
+# Source directory: /Users/center/relic/test
+# Build directory: /Users/center/relic/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_err "/Users/center/relic/bin/test_err")
+add_test(test_bn "/Users/center/relic/bin/test_bn")
+add_test(test_dv "/Users/center/relic/bin/test_dv")
+add_test(test_fp "/Users/center/relic/bin/test_fp")
+add_test(test_fpx "/Users/center/relic/bin/test_fpx")
+add_test(test_fb "/Users/center/relic/bin/test_fb")
+add_test(test_fbx "/Users/center/relic/bin/test_fbx")
+add_test(test_ep "/Users/center/relic/bin/test_ep")
+add_test(test_epx "/Users/center/relic/bin/test_epx")
+add_test(test_eb "/Users/center/relic/bin/test_eb")
+add_test(test_ed "/Users/center/relic/bin/test_ed")
+add_test(test_ec "/Users/center/relic/bin/test_ec")
+add_test(test_pc "/Users/center/relic/bin/test_pc")
+add_test(test_pp "/Users/center/relic/bin/test_pp")
+add_test(test_md "/Users/center/relic/bin/test_md")
+add_test(test_cp "/Users/center/relic/bin/test_cp")
+add_test(test_rand "/Users/center/relic/bin/test_rand")
+add_test(test_core "/Users/center/relic/bin/test_core")
